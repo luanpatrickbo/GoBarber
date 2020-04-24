054bf65f6388ac6dd1151e9680436b142d5f7bbf
luanpatrickbo <luanpatrickbo@gmail.com>
2020-04-23 20:39:42 -0400

    Configure post users

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get('/', async (req, res)=> {
-
-  const user = await User.create({
-    name: 'Luan Patrick',
-    email: 'luanpatrickbo@gmail.com',
-    password_hash: '123464664566'
-  });
-
-  return res.json(user)
-});
+routes.post('/users', UserController.store);
 
 export default routes;
