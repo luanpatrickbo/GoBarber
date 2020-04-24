9c7f029f5fcb89963b98123609e115ab55f72a0b
luanpatrickbo <luanpatrickbo@gmail.com>
2020-04-23 20:03:15 -0400

    Test for create User

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res)=> {
-  return res.json({massage: 'Hello cara'})
+routes.get('/', async (req, res)=> {
+
+  const user = await User.create({
+    name: 'Luan Patrick',
+    email: 'luanpatrickbo@gmail.com',
+    password_hash: '123464664566'
+  });
+
+  return res.json(user)
 });
 
 export default routes;
