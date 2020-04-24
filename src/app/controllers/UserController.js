634ccea15be0cac39591cbb3b60cf26ee02082fd
luanpatrickbo <luanpatrickbo@gmail.com>
2020-04-23 21:17:26 -0400

    Verify if user email User already exist

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,24 +2,25 @@ import User from '../models/User';
 
 
 class UserController {
-    async store(req, res){
-        const userExist = await User.findOne({ where: {email: req.body.email} });
+  async store(req, res){
+      
+    /* User exist */
+    
+    const userExist = await User.findOne({ where: {email: req.body.email} });
 
-        /* User exist */
-        
-        if(userExist){
-            return res.status(400).json({ error: 'User already exist' });
-        }
-
-        const { id, name, email, provider } = await User.create(req.body);
-        
-        return res.json({
-            id,
-            name,
-            email,
-            provider
-        });
+    if(userExist){
+        return res.status(400).json({ error: 'User already exist' });
     }
+    
+    const { id, name, email, provider } = await User.create(req.body);
+    
+    return res.json({
+        id,
+        name,
+        email,
+        provider
+    });
+  }
 }
 
 export default new UserController();
