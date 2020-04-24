d53eac49b571b3e3a20328778f71029c1d449302
luanpatrickbo <luanpatrickbo@gmail.com>
2020-04-24 08:54:06 -0400

    Validated users update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,10 +23,32 @@ class UserController {
   }
 
   async update(req, res){
-    console.log(req.userId);
-    
-    res.json({ message: true });
-  }
+   
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if(email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if(userExist) {
+        res.status(401).json({ error: 'User alredy exists' });
+      }
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))){
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider }  = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
+  } 
 }
 
 export default new UserController();
